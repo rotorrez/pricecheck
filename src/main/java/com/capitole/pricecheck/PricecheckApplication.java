eef81a6af993d449ca132d769a6a19c99cf4b212
Rommel <rommel.torrez@mercadolibre.com>
2022-12-10 11:17:43 -0300

    Initial commit

---
+++ src/main/java/com/capitole/pricecheck/PricecheckApplication.java
@@ -0,0 +1,13 @@
+package com.capitole.pricecheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PricecheckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PricecheckApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/capitole/pricecheck/PricecheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capitole.pricecheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PricecheckApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
